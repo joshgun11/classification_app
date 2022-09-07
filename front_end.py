2f2f2b70deaab53d8581d416106e0b0f365596fc
Joshgun <guliyejoshgun@gmail.com>
2022-09-07 16:28:23 +0200

    commit

---
+++ front_end.py
@@ -0,0 +1,46 @@
+
+import flasgger
+from flasgger import Swagger
+from flask import Flask,jsonify,request,render_template
+#from utilities import predict_model
+import keras
+import pickle
+import re
+#from tensorflow.keras.preprocessing.sequence import pad_sequences
+from utilities import predict_model
+import os
+
+
+app = Flask(__name__)
+Swagger(app)
+
+
+@app.route('/')
+def my_form():
+    return "Welcome All"
+
+@app.route('/predict',methods = ["Get"])
+def predict_text():
+    """Let's Classify the tweets 
+    This is using docstrings for specifications.
+    ---
+    parameters:  
+      - name: text
+        in: query
+        type: string
+        required: true
+    responses:
+        200:
+            description: The output values
+        
+    """
+
+    text = request.args.get('text')
+    return predict_model(text)
+    
+
+
+if __name__== '__main__':
+    #app.debug = True
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
---
+++ utilities.py
@@ -0,0 +1,71 @@
+
+import sys
+import keras
+import pickle
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+import re
+
+model_path='models/base_line/base_line.h5'
+tokenizer_path='models/base_line/tokenizer.pickle'
+
+model = keras.models.load_model(model_path)
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+
+with open(tokenizer_path, 'rb') as handle:
+    tokenizer = pickle.load(handle)
+
+
+
+def clean_text(x):
+    pattern = r'[^a-zA-z0-9\s]'
+    
+    text = re.sub(pattern, '', x)
+    text = re.sub(r'[^\w\s]', '', text)
+    return text
+
+def clean_numbers(x):
+    if bool(re.search(r'\d', x)):
+        x = re.sub('[0-9]{5,}', '#####', x)
+        x = re.sub('[0-9]**{4}**', '####', x)
+        x = re.sub('[0-9]**{3}**', '###', x)
+        x = re.sub('[0-9]**{2}**', '##', x)
+    return x
+
+def predict(text,model,tokenizer):
+
+    
+    #text = clean_text(text)
+    #text = clean_numbers(text)
+
+
+    seq = tokenizer.texts_to_sequences([text])
+    padded = pad_sequences(seq, maxlen=50)
+    pred = model.predict(padded)
+    predicted_class = pred.argmax(axis=-1)
+   
+    classes  = {'0':'Irrelevant', '1':'Negative', '2':'Neutral', '3':'Positive'}
+    actual_class = classes[str(predicted_class[0])]
+    result = {"text":text,"used model":"Base Line","prediction":actual_class}
+    return result
+
+
+def predict_model(text):
+    return predict(text,model,tokenizer)
+
+
+
+
+
+
+
+if __name__== "__main__":
+    #Text-to classify should be-in a list.
+   text = ["I love you"]
+   for i in text:
+    predictions = predict_model(i)
+    print(predictions)
+
+
+
+
